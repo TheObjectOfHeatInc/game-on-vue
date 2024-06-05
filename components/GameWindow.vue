98c9c0efec440019dbc7cae362baeb750f4356cc
Gerg Coop <TheObjectOfHeatInc@gmail.com>
2024-06-05 03:38:05 +0300

    first commit

---
+++ components/GameWindow.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="game-window">
+    <p>Это игровое окно</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'GameWindow',
+})
+</script>
+
+<style scoped>
+.game-window {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #000;
+  color: #fff;
+  box-sizing: border-box;
+}
+
+@media (max-width: 600px) {
+  .game-window {
+    height: 100vh;
+    padding: 10px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <GameWindow />
+  </div>
+</template>
+
+<script>
+import GameWindow from '~/components/GameWindow.vue'
+
+export default {
+  components: {
+    GameWindow,
+  },
+}
+</script>
